8858c99afd44e47bd3903033901ca4ded55903af
曾维涛 <weitao_zeng@jyc99.com>
2019-04-25 15:59:15 +0800

    再次测试提交的代码4.25

--- app/src/main/java/com/lhh/apst/advancedpagerslidingtabstrip/App.java
+++ app/src/main/java/com/lhh/apst/advancedpagerslidingtabstrip/App.java
@@ -18,6 +18,6 @@ public class App extends Application{
                 .newBuilder(this, new OkHttpClient())
                 .build();
         Fresco.initialize(this,frescoConfig);
-        System.out.println("");
+        System.out.println("打印");
     }
 }
